94a761e4dfb32af08c456d6c2016772dbdc9bb01
ulsa5353 <ulsa5353@email.com>
2022-06-09 10:45:54 +0530

    my first commit

---
+++ prime.py
@@ -0,0 +1,10 @@
+list1 = [5, 9, 8, 11, 15, 19, 25]
+prime_list = []
+for x in list1:
+    for i in range(2, (x//2)+1):
+        if x%i == 0:
+            break
+    else:
+        prime_list.append(x)
+    
+print(prime_list)
